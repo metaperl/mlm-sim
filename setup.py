540a11e399bc42b345623ded0f4e8c646a157730
Terrence Brannon <metaperl@gmail.com>
2022-02-27 02:54:33 -0500

    Make repo clean.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='mlm_sim',
     use_scm_version= {
-        'write_to': 'mlm_sim/metadata.py'
+        'write_to'w: 'mlm_sim/metadata.py'
     },
     setup_requires=['setuptools_scm'],
     packages=['mlm_sim'],
